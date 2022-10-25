c09a3b0542867ba1ee90143752ddc81a5e7a743d
Deepali gupta <95581953+Deepaligup-ta@users.noreply.github.com>
2022-10-25 16:32:21 +0530

    Initial commit

---
+++ src/content_option.js
@@ -0,0 +1,159 @@
+const logotext = "Deepali";
+const meta = {
+    title: "Deepali Gupta",
+    description: "I’m Deepali Gupta Cybersecurity Enthusiast Front-End Developer, Currently studing at UPES",
+};
+
+const introdata = {
+    title: "I’m Deepali Gupta",
+    animated: {
+        first: "I love Hacking",
+        second: "I code cool websites",
+        third: "I develop Front-End",
+    },
+    description: "I’m Deepali Gupta Cybersecurity Enthusiast Front-End Developer, Currently studing at UPES",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "About Myself",
+    aboutme: "Deepali Gupta 18 Year old Front End Developer, Pursuing B.Tech in cybersecurity from UPES. A Front-End Developer with the ability to develop high-quality websites and web applications by using latest technologies and web design trends.",
+};
+const worktimeline = [{
+        jobtitle: "Material Research",
+        where: "E-Cell-UPES",
+        date: "15 Oct 2022",
+    },
+    {
+    jobtitle: "Event Management",
+    where: "CSI-UPES",
+    date: "Sep21-July22",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "C",
+        value: 95,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "UI/UX",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Vulnerability Assesment",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Web Development",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1565249057942-6203205d93a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1597092430872-09a3f4338c60?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://images.unsplash.com/photo-1598751654513-bd903cff4b0b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1275&q=80",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1628166685220-3b9fa1388f19?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1291&q=80",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1615840788094-a4df00dce64b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1288&q=80",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "2003deepaligupta@Gmail.com.com",
+    YOUR_FONE: "7015060488",
+    description: " ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "",
+    facebook: "",
+    linkedin: "https://www.linkedin.com/in/deepali-gupta-03b239228/",
+    twitter: "",
+    youtube: "",
+    twitch: "",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
